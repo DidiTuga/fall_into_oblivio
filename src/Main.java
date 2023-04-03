f3bf15ef80d6fc8a6755db2f96c06f2f4971c6ff
diogo <diogo.manuel.santos@ubi.pt>
2023-04-03 21:19:23 +0100

    Ciclo While que esta constantemente a ver os ficheiros e aqueles que nao estao encriptados, encripta-os

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import javax.crypto.spec.IvParameterSpec;
+import javax.swing.*;
 
 import java.io.*;
 
@@ -12,49 +13,47 @@ public class Main {
         } catch (Exception exception) {}
 
         JFrame.setDefaultLookAndFeelDecorated(true);
-        hubWindow ola = new hubWindow();
+        hub ola = new hub();
         */
+
         IvParameterSpec iv = AESUtil.generateIv();
         // criar uma pasta chamada "pasta1"
         File pasta1 = new File("FALL-INTO-OBLIVION");
         pasta1.mkdir();
+        File[] ficheiros_enc;
+        while(true){
+            File[] ficheiros = pasta1.listFiles();
+            for (File ficheiro : ficheiros) {
+                // se o ficheiro for um ficheiro
+                if (ficheiro.isFile() && !ficheiro.getName().contains(".enc")) {
+                    // mostrar o nome do ficheiro
+                    try {
+                        System.out.println(ficheiro.getName());
+                        // ler o conteudo do ficheiro em bytes
+                        String nome = ficheiro.getName();
+                        // tirar a extensao do ficheiro
+                        if (nome.contains(".")){
+                            nome = nome.substring(0, nome.lastIndexOf("."));
+                        }
+                        // criar um ficheiro com o mesmo nome e a extensao .enc
+                        File ficheiro_enc = new File("FALL-INTO-OBLIVION/" + nome + ".enc");
+                        AESUtil.encryptFile("password", "salt", ficheiro, ficheiro_enc, iv);
 
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
 
+                }
+            }
+            // esperar 5 segundos
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         // ler os ficheiros da pasta em bytes
-        File[] ficheiros = pasta1.listFiles();
-        for (File ficheiro : ficheiros) {
-            // se o ficheiro for um ficheiro
-            if (ficheiro.isFile()) {
-                // mostrar o nome do ficheiro
-                try {
-                    System.out.println(ficheiro.getName());
-                    // ler o conteudo do ficheiro em bytes
-                    AESUtil.encryptFile("password", "salt", ficheiro, new File("FALL-INTO-OBLIVION/", "text.enc"), iv);
+        //AESUtil.decryptFile("password", "salt", ficheiro, new File("FALL-INTO-OBLIVION/", "text.txt"), iv);
 
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-
-        File[] ficheiros2 = pasta1.listFiles();
-        for (File ficheiro : ficheiros2) {
-            // se o ficheiro for um ficheiro
-            if (ficheiro.getName().contains(".enc")) {
-                // mostrar o nome do ficheiro
-                try {
-                    System.out.println(ficheiro.getName());
-                    // ler o conteudo do ficheiro em bytes
-                    AESUtil.decryptFile("password", "salt", ficheiro, new File("FALL-INTO-OBLIVION/", "text.txt"), iv);
-
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
     }
-
-
 }
